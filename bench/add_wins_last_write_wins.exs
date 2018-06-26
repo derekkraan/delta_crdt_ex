1a29ed07b27c9d045b16e65d85c2ad2d03640db2
Derek Kraan <derek.kraan@gmail.com>
2018-06-26 14:38:54 +0200

    Add regular Elixir map to benchmark

--- bench/add_wins_last_write_wins.exs
+++ bench/add_wins_last_write_wins.exs
@@ -35,17 +35,29 @@ with_100000 =
   end)
   |> SemiLattice.compress()
 
-Benchee.run(%{
-  "XS (100)" => fn ->
-    SemiLattice.join(with_100, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_100))
-  end,
-  "S (1000)" => fn ->
-    SemiLattice.join(with_1000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_1000))
-  end,
-  "M (10000)" => fn ->
-    SemiLattice.join(with_10000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_10000))
-  end,
-  "L (100000)" => fn ->
-    SemiLattice.join(with_100000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_100000))
-  end
-})
+normal_map_100000 =
+  1..100_000
+  |> Enum.reduce(map, fn x, map ->
+    Map.put(map, x, 1)
+  end)
+
+Benchee.run(
+  %{
+    "Elixir map L" => fn ->
+      Map.put(normal_map_100000, 91_919_191, 9_191_919_191)
+    end,
+    "XS (100)" => fn ->
+      SemiLattice.join(with_100, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_100))
+    end,
+    "S (1000)" => fn ->
+      SemiLattice.join(with_1000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_1000))
+    end,
+    "M (10000)" => fn ->
+      SemiLattice.join(with_10000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_10000))
+    end,
+    "L (100000)" => fn ->
+      SemiLattice.join(with_100000, AWLWWMap.add(91_919_191, 9_191_919_191, 1, with_100000))
+    end
+  },
+  print: [fast_warning: false]
+)
