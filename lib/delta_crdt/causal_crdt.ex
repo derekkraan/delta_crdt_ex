ae92db812e01eba4fde138279231043bd797dfb3
Derek Kraan <derek.kraan@gmail.com>
2018-07-09 13:24:52 +0200

    Move DeltaCrdt.Periodic to its own file.

--- lib/delta_crdt/causal_crdt.ex
+++ lib/delta_crdt/causal_crdt.ex
@@ -336,23 +336,3 @@ defmodule DeltaCrdt.CausalCrdt do
     end)
   end
 end
-
-defmodule DeltaCrdt.Periodic do
-  use GenServer
-
-  def start_link(message, interval) do
-    parent = self()
-    GenServer.start_link(__MODULE__, {parent, message, interval})
-  end
-
-  def init({parent, message, interval}) do
-    Process.send_after(self(), :tick, interval)
-    {:ok, {parent, message, interval}}
-  end
-
-  def handle_info(:tick, {parent, message, interval}) do
-    GenServer.call(parent, message, :infinity)
-    Process.send_after(self(), :tick, interval)
-    {:noreply, {parent, message, interval}}
-  end
-end
