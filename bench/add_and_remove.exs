f0d03e08f83e498d4cb408f5710bd3288351c310
Derek Kraan <derek.kraan@gmail.com>
2019-01-29 08:56:13 +0100

    Refine benchmark

--- bench/add_and_remove.exs
+++ bench/add_and_remove.exs
@@ -5,7 +5,7 @@ defmodule BenchHelper do
         result
 
       other ->
-        Process.sleep(500)
+        Process.sleep(50)
         get_result(fun, result)
     end
   end
@@ -13,19 +13,26 @@ end
 
 add = fn total ->
   fn ->
-    {:ok, crdt1} = DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 1)
-    {:ok, crdt2} = DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 1)
+    run = :rand.uniform(9_999_999)
+    {:ok, crdt1} = DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 50)
+    {:ok, crdt2} = DeltaCrdt.start_link(DeltaCrdt.AWLWWMap, sync_interval: 50)
     DeltaCrdt.add_neighbours(crdt1, [crdt2])
 
     Enum.each(1..total, fn x ->
       DeltaCrdt.mutate(crdt1, :add, [x, x])
     end)
 
-    BenchHelper.get_result(fn -> DeltaCrdt.read(crdt2) |> Map.get(total) end, total)
+    BenchHelper.get_result(
+      fn ->
+        DeltaCrdt.read(crdt2) |> Map.get(total)
+      end,
+      total
+    )
   end
 end
 
 Benchee.run(%{
-  "add 10_000 records" => add.(10_000),
-  "add 1_000 records" => add.(1000)
+  "add 1_000 records" => add.(1000),
+  "add 5_000 records" => add.(5000)
+  # "add 10_000 records" => add.(10_000)
 })
