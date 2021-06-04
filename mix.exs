a93bde9463b153efaaaffb2ce7a1247597c84855
Derek Kraan <derek.kraan@gmail.com>
2021-06-04 09:35:11 +0200

    Bump version to 0.6.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DeltaCrdt.MixProject do
   def project do
     [
       app: :delta_crdt,
-      version: "0.6.1",
+      version: "0.6.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
